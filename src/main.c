f5a4238e00dd80c61e0acca34af106e0851033cd
kurz-m <m.kurz92@gmail.com>
2023-08-28 08:09:11 +0200

    [main.c]: Update to integrate lexer

--- src/main.c
+++ src/main.c
@@ -8,15 +8,51 @@
 // executer
 // readline
 
+char* types[] = {
+	"TOKEN_WORD",
+	"TOKEN_FLAG",
+	"TOKEN_PIPE",
+	"TOKEN_LESS",
+	"TOKEN_GREATER",
+	"TOKEN_DLESS",
+	"TOKEN_DGREATER",
+	"TOKEN_EOF",
+	"TOKEN_QUOTE",
+	"TOKEN_DQUOTE",
+	"TOKEN_NEWLINE"
+};
+
+void	ft_print_token_list(t_lexer *lexer)
+{
+	int		len;
+	int		count;
+	t_type	type;
+
+	len = 0;
+	while(lexer->token_list != NULL)
+	{
+		count = 0;
+		type = ((t_token *)lexer->token_list->content)->type;
+		len = ((t_token *)lexer->token_list->content)->value_length;
+		printf("TOKEN: %s\t", types[type]);
+		while (len > count)
+		{
+			printf("%c", ((t_token *)lexer->token_list->content)->value[count]);
+			count++;
+		}
+		printf("\n-----------------------\n");
+		lexer->token_list = lexer->token_list->next;
+	}
+}
+
 int	main(int argc, char *argv[], char *envp[])
 {
 	t_main		main;
 
 	main = (t_main){};
 	init_shell(&main, envp);
-	print(STDOUT_FILENO, "HELLO");
-	env(main.env);
-	// while (TRUE)
-		// mainloop
+	main.lexer = ft_lexer("cat < test.txt | grep hello | wc > out.log");
+	ft_print_token_list(&main.lexer);
+
 	return (EXIT_SUCCESS);
 }
