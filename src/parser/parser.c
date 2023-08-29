d3465cd385d190df324b332a35747aab12030bd1
fabileo <76965192+leonfabi@users.noreply.github.com>
2023-08-29 13:29:42 +0200

    parsepipe function

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -2,18 +2,33 @@
 
 t_cmd	*parsepipe(t_cmd *cmd, t_dlist *head)
 {
+	t_pipecmd	*pipe;
+	t_pipecmd	*tmp;
+
+	pipe = NULL;
+	tmp =NULL;
 	while(((t_token *)head->content)->type != TOKEN_EOF)
 	{
 		if (((t_token *)head->content)->type == TOKEN_PIPE)
 		{
-				pipe = (t_pipecmd){
-					.type = PIPE,
-					.left = NULL,
-					.right = NULL
-				};
+			pipe = (t_pipecmd *)malloc(sizeof(t_pipecmd));
+			// if (pipe == NULL) Error handling
+			pipe->type = PIPE;
+			pipe->left = NULL;
+			pipe->right = NULL;
+			if (cmd == NULL)
+				cmd = (t_cmd *)pipe;
+			else
+			{
+				tmp = (t_pipecmd *)cmd;
+				while (tmp->right != NULL)
+					tmp = tmp->right;
+				tmp->right = pipe;
+			}
 		}
 		head = head->next;
 	}
+	return (cmd);
 }
 
 t_cmd	*ft_parser(t_lexer *lexer)
@@ -26,7 +41,7 @@ t_cmd	*ft_parser(t_lexer *lexer)
 	while(((t_token *)head->content)->type != TOKEN_EOF)
 	{
 		// search for pipe
-		tree_head = parsepipe(tree_head, head)
+		tree_head = parsepipe(tree_head, head);
 		// search for redirect
 		head = head->next;
 	}
