929e74ea95bda8a1e810a3930285013a2d8d0c91
Markus <markus.kurz@r-stahl.com>
2023-09-08 10:50:34 +0200

    update echo to the t_execcmd

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -5,31 +5,26 @@ int	print(int fd, char *str)
 	return (write(fd, str, ft_strlen(str)));
 }
 
-int	echo(char **arg)
+int	ft_echo(t_execcmd *cmd)
 {
-	t_bool	new_line;
-	int		count;
-	t_bool	print_check;
+	t_bool	nl_flag;
+	int		i;
+	int		arr_len;
 
-	count = 0;
-	print_check = FALSE;
-	new_line = TRUE;
-	if (arg[0] == NULL)
+	nl_flag = TRUE;
+	i = 0;
+	arr_len = ft_arrlen((const char **)cmd->argv);
+	if (NULL == cmd->argv[i])
 		return (print(STDOUT_FILENO, "\n"), TRUE);
-	if (ft_strcmp(arg[0], "-n") == 0)
+	if (ft_strncmp(cmd->argv[1], "-n", 3))
 	{
-		new_line = FALSE;
-		count++;
+		nl_flag = FALSE;
+		++i;
 	}
-	while (arg[count] != NULL)
-	{
-		if (print_check == TRUE)
-			print(STDOUT_FILENO, " ");
-		print(STDOUT_FILENO, arg[count]);
-		print_check = TRUE;
-		count++;
-	}
-	if (new_line == TRUE)
-		print(STDOUT_FILENO, "\n");
-	return (TRUE);
+	while (++i < arr_len - 1)
+		printf("%s ", cmd->argv[i]);
+	printf("%s", cmd->argv[i]);
+	if (nl_flag == TRUE)
+		printf("\n");
+	return (EXIT_SUCCESS);
 }
